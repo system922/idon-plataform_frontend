23864dc861f55bb4e331024c7caaa1bd80dd4d56
JeffG9907 <jcagua4477@utm.edu.ec>
2026-04-29 00:05:29 -0500

    Cambios en estructura

--- src/components/common/index.js
+++
@@ -1,4 +0,0 @@
-// Common Components
-export { default as Footer } from './Footer';
-export { default as PageTemplate } from './PageTemplate';
-export { default as ProtectedRoute } from './ProtectedRoute';
