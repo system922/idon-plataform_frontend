b55695525f0eac242603f77bfbf5d2d673f16d4e
JeffG9907 <jcagua4477@utm.edu.ec>
2026-04-30 11:26:12 -0500

    Agregar campos de validaciones en orden

--- src/pages/business/OrderPage.jsx
+++ src/pages/business/OrderPage.jsx
@@ -167,6 +167,7 @@ export default function TakeOrderPageNew() {
       setGuardando(false);
     }
   }
+  
   // ── Render ────────────────────────────────────────────────────────────────
 
   return (
